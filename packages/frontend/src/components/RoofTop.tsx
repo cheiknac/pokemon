dc934d6886b12d20f2a69025d769f9ac490ec7aa
cheiknac <cheikna.coulibaly@hotmail.fr>
2025-06-01 00:05:48 +0200

    add RoofTop componant

---
+++ packages/frontend/src/components/RoofTop.tsx
@@ -0,0 +1,12 @@
+import './css/RoofTop.css';
+
+const RoofTop = () => {
+    return (
+        <div id='topBar'>
+            <button className='bSignup'>Sign up</button>
+            <button className='bConexion'>Connexion</button>
+        </div>
+    )
+}
+
+export default RoofTop;
